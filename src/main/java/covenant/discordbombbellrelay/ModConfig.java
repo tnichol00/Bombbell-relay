292e138fdb9cb0021f7143b2fa24cd2fd3bbe816
tnichol00 <tnichol00@gmail.com>
2026-05-01 07:41:55 -0400

    Refactor ModConfig to use WEBHOOK_URL
    Updated ModConfig to include a static WEBHOOK_URL.

--- src/main/java/covenant/discordbombbellrelay/ModConfig.java
+++ src/main/java/covenant/discordbombbellrelay/ModConfig.java
@@ -2,13 +2,7 @@ package covenant.discordbombbellrelay;
 
 public class ModConfig {
 
-    private static String botUrl = "http://fi3.bot-hosting.net:22139/bomb";
+    // 🔧 PUT YOUR WEBHOOK URL HERE
+    public static String WEBHOOK_URL = "http://YOUR_SERVER_IP:22139/bomb";
 
-    public static String getBotUrl() {
-        return botUrl;
-    }
-
-    public static void setBotUrl(String url) {
-        botUrl = url;
-    }
 }
